6fdfa20cd58687d9cd0de161ee7b104cf691ce50
leand <leandrodasilva01921@gmail.com>
2023-05-24 22:49:08 -0300

    Fazendo Test na classe Pessoa

--- src/main/java/pescariaApi/classesSucessoras/Pessoa.java
+++ src/main/java/pescariaApi/classesSucessoras/Pessoa.java
@@ -1,4 +1,17 @@
 package pescariaApi.classesSucessoras;
 
 public class Pessoa {
+ String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String toString(){
+     return "nome: " + nome;
+ }
 }
