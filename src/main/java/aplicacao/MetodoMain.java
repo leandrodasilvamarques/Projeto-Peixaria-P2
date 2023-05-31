218dc67d16c07e007776a6c9403bf73b76ac9cd3
leandro.pimentel <leandrokhalelpimentel@gmail.com>
2023-05-31 11:05:30 -0300

    Leo

--- src/main/java/aplicacao/MetodoMain.java
+++ src/main/java/aplicacao/MetodoMain.java
@@ -1,5 +1,10 @@
 package aplicacao;
 
+import pescariaApi.embarcacao.Barco;
+import pescariaApi.embarcacao.Pescador;
+
+import javax.swing.*;
+
 public class MetodoMain {
     public static void main(String[] args) {
 
--- src/main/java/pescariaApi/Empresa/Pescaria.java
+++ src/main/java/pescariaApi/Empresa/Pescaria.java
@@ -3,8 +3,10 @@ package pescariaApi.Empresa;
 import pescariaApi.Empresa.financeiro.Financeiro;
 import pescariaApi.interfaces.Peixe;
 
-public class Pescaria {
+import java.util.List;
 
+public class Pescaria {
+    List<String>
     Financeiro financeiro;
     public double computarPesca(int numeroDePescadores, double quiloPescado, Peixe tipoDoPeixe){
         return 0.0;
