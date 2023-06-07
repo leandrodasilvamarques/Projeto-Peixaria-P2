11885d8e2c9b6feb0f25cb12ef4ca95bce34d8bb
leopimentl <leandrokhalelpimentel@gmail.com>
2023-06-07 12:59:55 -0300

    Criando os setores que a peixaria tem

--- src/main/java/classes/Barco.java
+++ src/main/java/classes/Barco.java
@@ -3,17 +3,48 @@ package classes;
 import classes.setores.SetorEmbarcacao;
 import controladores.Pescaria;
 
+import java.util.Objects;
+
 public class Barco {
 
-    private RegistroDePesca registro;
     private int id;
+    private RegistroDePesca registro;
 
 
     public Barco(int id) {
-
     }
 
     public RegistroDePesca getRegistro() {
         return registro;
+    }
+
+    public void setRegistro(RegistroDePesca registro) {
+        this.registro = registro;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Barco barco = (Barco) o;
+        return id == barco.id && Objects.equals(registro, barco.registro);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(registro, id);
+    }
+
+    @Override
+    public String toString() {
+        return "Barco{" + "id=" + id + ", registro=" + registro + '}';
     }
 }
