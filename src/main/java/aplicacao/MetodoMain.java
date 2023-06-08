caf3318504faeb27bc62b9a2500cca4d996cc29f
leopimentl <leandrokhalelpimentel@gmail.com>
2023-06-08 16:49:00 -0300

    Metodo main e UserInterface

--- src/main/java/aplicacao/MetodoMain.java
+++ src/main/java/aplicacao/MetodoMain.java
@@ -1,34 +1,27 @@
 package aplicacao;
 
-import classes.setores.embarcacao.Barco;
-import classes.setores.empresa.Pescaria;
+import classes.setores.recursosHumanos.Gerente;
 
-import java.util.Scanner;
+import javax.swing.*;
 
 public class MetodoMain {
 
-    public static void printPainelInicial(){
-        System.out.println("Digite uma das opções:\n" +
-                "[1] Login como adminstador\n" +
-                "[2] Login usuario");
-    }
-    public static int respostaPainelInicial(){
-        Scanner input = new Scanner(System.in);
+    static UserInterface UI = new UserInterface();
 
-        int resposta = input.nextInt();
-        input.nextLine();
-        return resposta;
-    }
-
-    public static void decisaoPainelInicial(int resposta){
-        switch (resposta){
-            case (1) -> System.out.println("Login com admin");
-
-            case (2) -> System.out.println("Login como usuario");
-        }
-    }
     public static void main(String[] args) {
-        printPainelInicial();
-        decisaoPainelInicial(respostaPainelInicial());
+
+        boolean autenticado = false;
+        do {
+            String login = JOptionPane.showInputDialog(null, "Login:");
+            String senha = JOptionPane.showInputDialog(null, "Senha:");
+
+            if (new Gerente().autenticar(login, senha)) {
+                autenticado = true;
+                UI.printaTelaPrincipal();
+            } else {
+                JOptionPane.showMessageDialog(null, "Login ou senha inválidos. Tente novamente.");
+            }
+
+        } while (!autenticado);
     }
 }
---
+++ src/main/java/aplicacao/UserInterface.java
@@ -0,0 +1,33 @@
+package aplicacao;
+
+// classe User Interface, com os métodos para printar na tela
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class UserInterface {
+
+    public void printaTelaPrincipal(){
+        JOptionPane.showInputDialog(null, "[1]\n" +
+                "[2]\n" +
+                "[3]\n" +
+                "[4]\n" +
+                "[5]\n");
+    }
+
+    public int respostaPainelInicial(){
+        Scanner input = new Scanner(System.in);
+
+        int resposta = input.nextInt();
+        input.nextLine();
+        return resposta;
+    }
+
+    public void decisaoPainelInicial(int resposta){
+        switch (resposta){
+            case (1) -> System.out.println("Login com admin");
+
+            case (2) -> System.out.println("Login como usuario");
+        }
+    }
+}
