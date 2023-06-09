696670485bd2d932d48d9ddce944dc901a8b39c2
leopimentl <leandrokhalelpimentel@gmail.com>
2023-06-09 01:03:29 -0300

    Metodo main e UserInterface

--- src/main/java/aplicacao/UserView.java
+++ src/main/java/aplicacao/UserView.java
@@ -2,10 +2,8 @@ package aplicacao;
 
 // classe User Interface, com os métodos para printar na tela
 
-import classes.setores.recursosHumanos.Gerente;
 
 import javax.swing.*;
-import java.util.Scanner;
 
 public class UserView {
 
@@ -15,37 +13,5 @@ public class UserView {
                 "[3]\n" +
                 "[4]\n" +
                 "[5]\n");
-    }
-
-    public int respostaPainelInicial(){
-        Scanner input = new Scanner(System.in);
-
-        int resposta = input.nextInt();
-        input.nextLine();
-        return resposta;
-    }
-
-    public void decisaoPainelInicial(int resposta){
-        switch (resposta){
-            case (1) -> System.out.println("Login com admin");
-
-            case (2) -> System.out.println("Login como usuario");
-        }
-    }
-
-    public void autenticadorDeUsuario(UserInterface UI){
-        boolean autenticado = false;
-        do {
-            String login = JOptionPane.showInputDialog(null, "Login:");
-            String senha = JOptionPane.showInputDialog(null, "Senha:");
-
-            if (new Gerente().autenticar(login, senha)) {
-                autenticado = true;
-                UI.printaTelaPrincipal();
-            } else {
-                JOptionPane.showMessageDialog(null, "Login ou senha inválidos. Tente novamente.");
-            }
-
-        } while (!autenticado);
     }
 }
