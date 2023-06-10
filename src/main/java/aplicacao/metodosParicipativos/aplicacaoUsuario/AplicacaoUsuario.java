c9a1986cd48351b3b2889b71e520e36768edf389
leand <leandrodasilva01921@gmail.com>
2023-06-09 21:24:03 -0300

    aplicacao ususario com os cases

--- src/main/java/aplicacao/metodosParicipativos/aplicacaoUsuario/AplicacaoUsuario.java
+++ src/main/java/aplicacao/metodosParicipativos/aplicacaoUsuario/AplicacaoUsuario.java
@@ -1,12 +1,55 @@
 package aplicacao.metodosParicipativos.aplicacaoUsuario;
 
+import aplicacao.metodosParicipativos.aplicacaoAdmin.UserViewAdmin;
 import aplicacao.metodosParicipativos.interacoesIniciais.UserViewInicial;
+import classes.setores.embarcacao.Barco;
+import classes.setores.empresa.Pescaria;
+
+import java.util.Scanner;
 
 public class AplicacaoUsuario {
 
-    public static void aplicacaoUsuario() {
+    public static void AplicacaoUsuario() {
 
-        UserViewInicial.printOpcoesUsuario();
+        UserViewUsuario.printOpcoes();
+        decisaoOpcoesAdmin(recebeInt());
 
     }
+
+    public static void decisaoOpcoesAdmin(int opcao){
+
+        final int REALIZAR_COMPRAS = 1;
+        final int VISUALIZAR_PRECOS = 2;
+        final int SIMULAR_DESCONTO = 3;
+
+        switch (opcao){
+            case REALIZAR_COMPRAS ->{
+
+            }
+
+
+            case VISUALIZAR_PRECOS->{
+
+            }
+
+            case SIMULAR_DESCONTO->{
+
+            }
+        }
+
+
+
+
+
+    public static int recebeInt(){
+
+        Scanner input = new Scanner(System.in);
+
+        int opcao = input.nextInt();
+        input.nextLine();
+
+        return opcao;
+    }
+}
+
 }
--- src/main/java/aplicacao/metodosParicipativos/aplicacaoUsuario/UserViewUsuario.java
+++ src/main/java/aplicacao/metodosParicipativos/aplicacaoUsuario/UserViewUsuario.java
@@ -1,4 +1,11 @@
 package aplicacao.metodosParicipativos.aplicacaoUsuario;
 
 public class UserViewUsuario {
+
+    public static void printOpcoesUsuario(){
+
+        System.out.println("[1] Realizar Compra\n" +
+                "[2] Ver Preços\n" +
+                "[3] Simular Desconto a cada 10kg");
+    }
 }
