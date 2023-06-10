ed5a73e6566a4f8a3f139559d7279dd8938ac362
leand <leandrodasilva01921@gmail.com>
2023-06-09 21:36:20 -0300

    asdf

--- src/main/java/aplicacao/metodosParicipativos/aplicacaoUsuario/AplicacaoUsuario.java
+++ src/main/java/aplicacao/metodosParicipativos/aplicacaoUsuario/AplicacaoUsuario.java
@@ -1,12 +1,46 @@
 package aplicacao.metodosParicipativos.aplicacaoUsuario;
 
-import aplicacao.metodosParicipativos.interacoesIniciais.UserViewInicial;
+import java.util.Scanner;
 
 public class AplicacaoUsuario {
 
-    public static void aplicacaoUsuario() {
+    public static int recebeInt(){
 
-        UserViewInicial.printOpcoesUsuario();
+        Scanner input = new Scanner(System.in);
+
+        int opcao = input.nextInt();
+        input.nextLine();
+
+        return opcao;
+    }
+
+    public static void AplicacaoUsuario() {
+
+        UserViewUsuario.printOpcoesUsuario();
+        decisaoOpcoesAdmin(recebeInt());
 
     }
+
+    public static void decisaoOpcoesAdmin(int opcao){
+
+        final int REALIZAR_COMPRAS = 1;
+        final int VISUALIZAR_PRECOS = 2;
+        final int SIMULAR_DESCONTO = 3;
+
+        switch (opcao){
+            case REALIZAR_COMPRAS ->{
+
+            }
+
+
+            case VISUALIZAR_PRECOS->{
+
+            }
+
+            case SIMULAR_DESCONTO->{
+
+            }
+        }
+}
+
 }
